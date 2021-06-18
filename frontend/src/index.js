76e0cff97c58dc211c84a4eb749872ab7aeec71f
IndraP24 <indrashis985@gmail.com>
2021-06-19 00:24:36 +0530

    Re-initialize react app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,16 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { jsx } from '@emotion/react'
-import { ThemeProvider  } from '@chakra-ui/core';
-
-import Header from './components/Header';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function App() {
-  return (
-    <ThemeProvider>
-      <Header />
-    </ThemeProvider>
-  )
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-const rootElement = document.getElementById("root")
-render(<App />, rootElement)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
