9bedcb5ac5291e700498fbbae04cb305fa8896af
IndraP24 <indrashis985@gmail.com>
2021-06-20 00:37:54 +0530

    Rendered the changes from TodoList

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { Heading, VStack } from '@chakra-ui/react';
+import { FaSun, FaMoon } from 'react-icons/fa';
+import { Heading, VStack, IconButton } from '@chakra-ui/react';
 import Todos from "./components/TodoList";
+import AddTodo from "./components/AddTodo";
 
 function App() {
   return (
-    <VStack>
-      <Heading>Todo Application</Heading>
+    <VStack p={4}>
+      <IconButton 
+        icon={<FaMoon />} 
+        isRound="true" 
+        size="lg" 
+        alignSelf="flex-end" 
+      />
+
+      <Heading 
+        fontWeight="extrabold" 
+        size="2xl" 
+        bgGradient="linear(to-r, pink.500, pink.300, blue.500)"
+        bgClip="text"
+        p={10}
+      >
+          Todo Application
+      </Heading>
+
       <Todos />
+      <AddTodo />
     </VStack>
   );
 }
