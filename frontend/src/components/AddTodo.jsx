dc75eecfc3521b6798095fadd8c7e8f73780ffe4
IndraP24 <indrashis985@gmail.com>
2021-06-20 21:05:01 +0530

    Revert back changes

--- frontend/src/components/AddTodo.jsx
+++ frontend/src/components/AddTodo.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Input, HStack } from "@chakra-ui/react";
+import { Input, HStack, Button } from "@chakra-ui/react";
 
 const TodosContext = React.createContext({
     todos: [], fetchTodos: () => {}
@@ -12,31 +12,35 @@ function AddTodo() {
 
     const handleInput = event => {
         setItem(event.target.value)
-    }
+    };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+    const handleSubmit = () => {
         const newTodo = {
             "id": todos.length + 1,
             "item": item
         }
-
+        
         fetch("http://localhost:8000/todo", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(newTodo)
         }).then(fetchTodos)
-    }
+    };
 
     return (
         <form onSubmit={handleSubmit}>
             <HStack mt="8">
                 <Input
                     variant="filled"
+                    type="text"
                     placeholder="Add a todo item"
                     aria-label="Add a todo item"
+                    value={item}
                     onChange={handleInput}
                 />
+                <Button colorScheme="pink" px="8" type="submit">
+                    Add Todo
+                </Button>
             </HStack>
         </form>
     );
